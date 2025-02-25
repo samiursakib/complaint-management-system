37960972e21f86e1263b72bd70c76a928d124aa7
samiursakib <rafisamiur@gmail.com>
2025-02-26 04:21:14 +0600

    user login and session management completed

--- server/index.js
+++ server/index.js
@@ -4,12 +4,25 @@ const process = require("process");
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/user", require("./routes/user.js"));
+app.use("/complaint", require("./routes/complaint.js"));
+
 const PORT = process.env.PORT || 4000;
 
 app.get("/ping", async (req, res) => {
-  const result = await DB.query("SELECT 1 + 1 AS solution");
-  console.log(result);
   res.send("pong");
+});
+
+app.post("/login", async (req, res) => {
+  const query = `SELECT * FROM users WHERE email = ? AND password = ?`;
+  const result = await DB.query(query, [req.body.email, req.body.password]);
+  if (result.length) {
+    return res.json(result[0]);
+  }
+  return null;
 });
 
 app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
